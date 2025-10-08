63eb0128f421afccd2c7924bbb9c98ca474b7399
kalpa <kalpa@DESKTOP-8BMI73P>
2025-10-08 19:24:39 +0530

    Initial Commit

--- ecobazaar/src/main/java/com/ecobazaar/ecobazaar/EcoBazaarAppApplication.java
+++ ecobazaar/src/main/java/com/ecobazaar/ecobazaar/EcoBazaarAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcoBazaarAppApplication {
-
+//first commit
 	public static void main(String[] args) {
 		SpringApplication.run(EcoBazaarAppApplication.class, args);
 	}
